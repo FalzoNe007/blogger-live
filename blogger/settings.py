9636a0dde2dba5bf69fe5a4d5e20ff29aaee3509
FalzoNe007 <59187596+FalzoNe007@users.noreply.github.com>
2022-01-08 19:13:01 +0100

    Update settings.py

--- blogger/settings.py
+++ blogger/settings.py
@@ -126,6 +126,8 @@ STATIC_URL = 'static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
